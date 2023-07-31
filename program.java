1e8329e481f6e40b3624c3a712fe51bcfca15f6d
07091983 <126685467+07091983@users.noreply.github.com>
2023-07-31 20:52:54 +0700

    Update program.java

--- program.java
+++ program.java
@@ -1,9 +1,3 @@
-// import java.util.ArrayList;
-// import java.util.Arrays;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.Map;
-
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
